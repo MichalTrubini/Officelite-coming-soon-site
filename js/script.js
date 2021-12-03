4487b32f1317d949c043ddec3fcb594745b5252d
Michal Trubini <mchl.trubini@gmail.com>
2021-12-03 21:19:12 +0100

    js, countdown WIP

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 //GET FUTURE DATE
 const days = 30;
-let secondsPerDay = 24 * 60 * 60 * 1000;
-let nextDate = new Date( Date.now() + days * secondsPerDay);
+let miliSecondsPerDay = 24 * 60 * 60 * 1000;
+let nextDate = new Date( Date.now() + days * miliSecondsPerDay);
+let secondsTotal = (days * miliSecondsPerDay) / 1000;
 
 let	day = nextDate.getUTCDate(),
 	months = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'),
@@ -13,8 +14,24 @@ document.querySelector('.countdown__date--accent').innerText = futureDate;
 
 //COUNTDOWN
 
-let secondsTotal = days * secondsPerDay;
+setInterval(countdown,1000);
 
-let numberOfDays = secondsTotal / secondsPerDay;
+function countdown() {
 
-console.log(numberOfDays);
+let secondsPerDay = 86400;
+let secondsPerhour = 3600;
+let secondsPerMinute = 60;
+
+let numberOfDays = Math.floor(secondsTotal / secondsPerDay);
+let numberOfHours = Math.floor((secondsTotal % secondsPerDay) / secondsPerhour );
+let numberOfMinutes = Math.floor(((secondsTotal % secondsPerDay) % secondsPerhour ) / secondsPerMinute);
+let numberOfSeconds = ((secondsTotal % secondsPerDay) % secondsPerhour ) % secondsPerMinute;
+
+document.getElementById('remainder-days').innerText = numberOfDays;
+document.getElementById('remainder-hours').innerText = numberOfHours;
+document.getElementById('remainder-minutes').innerText = numberOfMinutes;
+document.getElementById('remainder-seconds').innerText = numberOfSeconds;
+
+console.log(numberOfDays,numberOfHours, numberOfMinutes,numberOfSeconds);
+secondsTotal--;
+}

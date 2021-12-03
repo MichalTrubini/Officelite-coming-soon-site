780c27cb2928ccfabd0aef7146c365587ff1c3c2
Michal Trubini <mchl.trubini@gmail.com>
2021-12-03 19:01:34 +0100

    js, future date

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-//GET CURRENT DATE
+//GET FUTURE DATE
+const days = 30;
+let secondsPerDay = 24 * 60 * 60 * 1000;
+let nextDate = new Date( Date.now() + days * secondsPerDay);
 
-let objToday = new Date(),
-	day = objToday.getDay(),
+let	day = nextDate.getUTCDate(),
 	months = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'),
-	curMonth = months[objToday.getMonth()],
-	curYear = objToday.getFullYear(),
-    today = day + " " + curMonth + " " + curYear;
+	curMonth = months[nextDate.getMonth()],
+	curYear = nextDate.getFullYear(),
+    futureDate = day + " " + curMonth + " " + curYear;
 
-document.querySelector('.countdown__date--accent').innerText = today;
+document.querySelector('.countdown__date--accent').innerText = futureDate;
+
+//COUNTDOWN
+
+let secondsTotal = days * secondsPerDay;
+
+let numberOfDays = secondsTotal / secondsPerDay;
+
+console.log(numberOfDays);

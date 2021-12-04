32880f288a7ee8f6aaf2f3f6f111b30072251e43
Michal Trubini <mchl.trubini@gmail.com>
2021-12-04 14:36:23 +0100

    js, form validation WIP

--- js/script.js
+++ js/script.js
@@ -72,16 +72,20 @@ selectOption.addEventListener('click', function(){
     selectPackage.forEach(function(item){
 
         item.addEventListener('click', function(){
+
             let checkmarkCurrent = item.querySelector('.sign-up__list-group-checkmark');
             let checkmarksAll = document.querySelectorAll('.sign-up__list-group-checkmark');
-
             checkmarksAll.forEach(function(checkmarkPrevious){
                 if (checkmarkPrevious.classList.contains('show-element') && checkmarkPrevious.getAttribute('data-selected') !== checkmarkCurrent.getAttribute('data-selected')) checkmarkPrevious.classList.remove('show-element');
             })
-            
             checkmarkCurrent.classList.toggle('show-element');
 
-            //ADD SELECTION TO FORM
+            let packMenu = document.querySelector('.sign-up__list-group');
+            let arrow = document.querySelector('.sign-up__arrow');
+            packMenu.classList.remove('show-element');
+            arrow.classList.remove('arrow-up');
+
+            //ADD SELECTION TO THE FORM
 
             let packageNameSelected = item.querySelector('.sign-up__package-name').innerText;
             let packagePriceSelected = item.querySelector('.sign-up__package-price').innerText;
@@ -91,8 +95,51 @@ selectOption.addEventListener('click', function(){
 
             packageNameForm.innerText = packageNameSelected;
             packagePriceForm.innerText = packagePriceSelected;
-
         })
     })
-
 })();
+
+(function () {
+    let submitButton = document.querySelector('#submit-form');
+    
+    submitButton.addEventListener('click',errorAll);
+    })();
+    
+    function errorAll() {
+    
+      let input = document.querySelectorAll('input');
+      
+      input.forEach(errorSpecific);
+      input.forEach(checkIfFixed);
+    
+      function errorSpecific(item) {
+        if(!item.validity.valid) {
+          let dataAttribute = item.getAttribute('data-errorId');
+          let errorIcon = document.querySelector(`.contact__error-icon[data-errorId='${dataAttribute}']`);
+          let errorMessage = document.querySelector(`.contact__error-text[data-errorId='${dataAttribute}']`);
+    
+          item.classList.add('input-error');
+          errorIcon.classList.add('contact__error-icon-visible');
+          errorMessage.innerText = errorMessages[dataAttribute];
+          errorMessage.classList.add('contact__error-text-visible');
+        }
+      }
+    
+      function checkIfFixed(item) {
+        item.addEventListener('input',checker);
+    
+        function checker(){
+          let dataAttribute = item.getAttribute('data-errorId');
+          let errorIcon = document.querySelector(`.contact__error-icon[data-errorId='${dataAttribute}']`);
+          let errorMessage = document.querySelector(`.contact__error-text[data-errorId='${dataAttribute}']`);
+    
+          if(!item.validity.valid) item.classList.add('input-error');
+          if(item.validity.valid) {
+            item.classList.remove('input-error');
+            errorIcon.classList.remove('contact__error-icon-visible');
+            errorMessage.innerText = '';
+            errorMessage.classList.remove('contact__error-text-visible');
+          }
+        }
+      }
+    }
